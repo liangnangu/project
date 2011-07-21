62b28050d891fbc9a0ca45c7fbd8414fbf3837fc
liangnangu <574647990@qq.com>
2011-07-21 11:19:02 +0800

    宏定义，插入模式 ctrl + n  自动补齐

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+#define how_many_times_to_say_hello 1000
 int main (void)
 {
+	int i;
+	for (i = 0; i < how_many_times_to_say_hello; i++) {	
 		printf("hello word \n");
-		return 0;
+		}
+	return 0;
 }
+
